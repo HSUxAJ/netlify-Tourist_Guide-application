8fe59c74f25a185908137a024b08b4403decd401
HSUxAJ <79509075+HSUxAJ@users.noreply.github.com>
2023-05-30 19:52:25 +0800

    update

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-window.onload = function() {
-  // 解析網址中的查詢參數
-  const urlParams = new URLSearchParams(window.location.search);
-  const userId = urlParams.get('userId');
-  const userName = urlParams.get('userName');
-
-  // 顯示 User ID 和 User Name
-  const userInfoDiv = document.getElementById('userInfo');
-  userInfoDiv.innerHTML = `User ID: ${userId}<br>User Name: ${userName}`;
-};
+const list = document.getElementById('list');
+const input = document.getElementById('person-input');
 
 // 初始化 Firebase
 const firebaseConfig = {
@@ -24,6 +16,11 @@ firebase.initializeApp(firebaseConfig);
 
 // 獲取 Firestore 資料庫實例
 var db = firebase.firestore();
+
+// 解析網址中的查詢參數
+const urlParams = new URLSearchParams(window.location.search);
+window.userId = urlParams.get('userId');
+window.userName = urlParams.get('userName');
 
 // 啟用表單輸入
 function enableForm() {
@@ -42,19 +39,13 @@ userForm.addEventListener('submit', function(event) {
   var birthday = document.getElementById('birthday').value;
   var email = document.getElementById('email').value;
 
-  // 獲取之前儲存的姓名和使用者 ID
-  var hiddenNameInput = document.getElementById('hiddenName');
-  var hiddenUserIdInput = document.getElementById('hiddenUserId');
-  var hiddenName = hiddenNameInput.value;
-  var hiddenUserId = hiddenUserIdInput.value;
-
   // 在控制台輸出資料
   console.log('姓名:', name);
   console.log('公司:', company);
   console.log('生日:', birthday);
   console.log('信箱:', email);
-  console.log('Line姓名', userName);
-  console.log('Line UID', userId);
+  console.log('Line姓名', window.userName);
+  console.log('Line UID', window.userId);
 
   // 將資料儲存到 Firestore
   db.collection('Tourist_Guide').add({
@@ -63,8 +54,8 @@ userForm.addEventListener('submit', function(event) {
     birthday: birthday,
     email: email,
     qualified: true,
-    line_name: userName,
-    line_userId: userId
+    line_name: window.userName,
+    line_userId: window.userId
   })
     .then(function(docRef) {
       console.log('文件已成功添加，ID:', docRef.id);
@@ -78,6 +69,3 @@ userForm.addEventListener('submit', function(event) {
       console.error('儲存資料時出現錯誤:', error);
     });
 });
-
-// 呼叫初始化函式
-initializeLineLogin();
