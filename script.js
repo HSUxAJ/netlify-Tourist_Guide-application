33f9df1e1ce1ca94779ea39e729b0ac907855f63
HSUxAJ <79509075+HSUxAJ@users.noreply.github.com>
2023-05-30 10:46:59 +0800

    update

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-// 取得 URL 中的查詢參數
-function getUrlParameter(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  var results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-// 獲取 Line User ID 並顯示在網頁上
-function showUserId() {
-  var userId = getUrlParameter('userId');
-  document.getElementById('userId').innerText = 'Line User ID: ' + userId;
-}
-
-// 在網頁載入完成後執行
 window.onload = function() {
-  showUserId();
+  // 解析網址中的查詢參數
+  const urlParams = new URLSearchParams(window.location.search);
+  const userId = urlParams.get('userId');
+  const userName = urlParams.get('userName');
+
+  // 顯示 User ID 和 User Name
+  const userInfoDiv = document.getElementById('userInfo');
+  userInfoDiv.innerHTML = `User ID: ${userId}<br>User Name: ${userName}`;
 };
